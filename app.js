29fa5f8c3d67198c3107e0aa2730c83c72334eb1
Qasim Ali <qasimali24@gmail.com>
2020-06-09 08:31:08 +0500

    index file is prepared

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const readXlsxFile = require('read-excel-file/node');
+const hbs = require('hbs');
+const app = express()
+
+hbs.registerHelper("changeToPrettyDate",function(value) {
+  console.log(value.toString());
+  let array = value.toString().split(' ');
+  console.log(array);
+  let day = array[2];
+  let month = array[1];
+  let year = array[3];
+  let connectDate = day + ' ' + month + ' ' + year;
+  console.log(connectDate);
+  return connectDate;
+})
+
+app.get('/', function (req, res) {
+  // code to read data from excel sheet
+  // File path.
+  readXlsxFile('./public/data.xlsx').then((rows) => {
+    // `rows` is an array of rows
+    // each row being an array of cells.
+    res.render('index.hbs', {
+      cards: rows
+    });
+  }).catch(e => {
+    console.log(e);
+  })
+
+
+})
+
+app.listen(3000)
