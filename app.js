4e16b251320bcfc5aeef6f874085465fe7deff3c
Sentinel-25 <SharjeelAsif.25@gmail.com>
2020-06-14 21:54:53 +0500

    Sir need to discuss error tomorrow pl.

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.get('/', function(req, res) {
       console.log(val);
       let className = newFunction(val[2]);
       val.push(className);
+      let newvalue = newFunction3(val[3])
+      val.push(newvalue);
       // console.log(val);
       return val;
     })
@@ -57,16 +59,45 @@ let newFunction = function(valtwo) {
   let convertIntoDays = numberOfDaysSinceLastLve / (1000 * 60 * 60 * 24); // 167.9080978 days
 
   // if number of days since last leave > 90 days --- give this guyy the leave
-  console.log({convertIntoDays});
+  //console.log({convertIntoDays});
   if (convertIntoDays > 90) {
-    console.log('change the color of the card shown in website into red.');
+    //console.log('change the color of the card shown in website into red.');
     return 'red';
   } else {
     // if < 90 days -- show how many days left until next leave
-    console.log('only show remaing days until next leave');
+    //console.log('only show remaing days until next leave');
     return '';
   }
 }
+
+let newFunction3 = function(valthree) {
+
+  let today = new Date().getTime(); // 08932498172349812734 these are milliseconds since 1970
+
+// how many days since last leave?
+
+// last leave - today = number of days since last leave
+
+let lastLeave = new Date(valthree).getTime(); // 981740189723401892374
+
+let numberOfDaysSinceLastLve = today - lastLeave; // 890234871236
+
+let convertIntoDays = numberOfDaysSinceLastLve / (1000 * 60 * 60 * 24); // 167.9080978 days
+
+// if number of days since last leave > 90 days --- give this guyy the leave
+//console.log({convertIntoDays});
+if (convertIntoDays > 90) {
+    console.log('change the color of the card shown in website into red.');
+    return convertIntoDays;
+    }
+else {
+    // if < 90 days -- show how many days left until next leave
+    console.log('only show remaing days until next leave');
+    return convertIntoDays;
+    }
+}
+
+
 
 
 // a new route has been added at this point
--- sharjeel.js
+++ sharjeel.js
@@ -1,1 +1,23 @@
 // call a function and get a value out of it. show in console.table
+let today = new Date().getTime(); // 08932498172349812734 these are milliseconds since 1970
+
+// how many days since last leave?
+
+// last leave - today = number of days since last leave
+
+let lastLeave = new Date(valtwo).getTime(); // 981740189723401892374
+
+let numberOfDaysSinceLastLve = today - lastLeave; // 890234871236
+
+let convertIntoDays = numberOfDaysSinceLastLve / (1000 * 60 * 60 * 24); // 167.9080978 days
+
+// if number of days since last leave > 90 days --- give this guyy the leave
+console.log({convertIntoDays});
+if (convertIntoDays > 90) {
+  console.log('change the color of the card shown in website into red.');
+  return convertIntoDays;
+} else {
+  // if < 90 days -- show how many days left until next leave
+  console.log('only show remaing days until next leave');
+  return convertIntoDays;
+}
